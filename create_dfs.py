ebe8770260a56297a858fd9f23380d847dec4151
Alexandr Kravchenko <mrkom1mail@gmail.com>
2021-01-04 14:43:18 +0200

    reworked create_dfs for 0.6.X tracker API

--- create_dfs.py
+++ create_dfs.py
@@ -8,25 +8,22 @@ import numpy as np
 def read_df(path_files, cols=['gazePointX', 'gazePointY', 'timestamp']):
     csv_files = [os.path.join(path, name) for path, subdirs,
              files in os.walk(path_files) for name in files if name.endswith(".csv")]
-
     dfs = pd.DataFrame(columns=cols)
 
     for name in csv_files:
         df = pd.read_csv(name)
-        # 0.4.0 tracker
-        if cols[0] in df.columns:
-            json_name = name[:-11]+"structure.json"
-            with open(json_name, "r") as read_file:
-                data = json.load(read_file)
-                width = data[0]['screen']['width']
-                height = data[0]['screen']['height']
+        df = df[['BPOGX', 'BPOGY', 'TIMESTAMP']]
+        df = df.rename(columns={"BPOGX": "gazePointX",
+                                "BPOGY": "gazePointY",
+                                "TIMESTAMP": "timestamp"})
+        meta_name = name[:-10]+"meta.json"
+        with open(meta_name, "r") as read_file:
+            data = json.load(read_file)
+            width = data['screen']['width']
+            height = data['screen']['height']
             df = df[cols]
             df['gazePointX'] = df.gazePointX / width
             df['gazePointY'] = df.gazePointY / height
-        # older tracker
-        else:
-            df = df[['gx', 'gy', 'timestamp']]
-            df = df.rename(columns={"gx": "gazePointX", "gy": "gazePointY"})
         df["Name"] = "_".join(name.split("/")[-3:-1])
         dfs = pd.concat([dfs, df])
 
@@ -35,8 +32,8 @@ def read_df(path_files, cols=['gazePointX', 'gazePointY', 'timestamp']):
 
 
 if __name__ == '__main__':
-    df_reading = read_df("data/reading")
-    df_non_reading = read_df("data/non-reading")
+    df_reading = read_df("cloud/reading/sessions")
+    df_non_reading = read_df("cloud/non-reading/sessions")
 
-    df_reading.to_pickle("data/data_reading.pkl")
-    df_non_reading.to_pickle("data/data_non_reading.pkl")
+    # df_reading.to_pickle("data/data_reading.pkl")
+    # df_non_reading.to_pickle("data/data_non_reading.pkl")
